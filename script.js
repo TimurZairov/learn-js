02d336fca13711735a206e2fb4c73d8da8f52be9
Timur Zairov <zairovne@gmail.com>
2021-01-31 21:52:07 +0500

    first comit

---
+++ script.js
@@ -0,0 +1,12 @@
+let money = 600;
+let income = 200;
+let addExpenses = 300;
+const deposit = 800;
+let mission, period;
+
+mission = 'Накопить 5000$';
+period = 'За 4 месяца';
+
+alert('Изучение java script гит непонятен');
+
+console.log('Тяжело в ученье легко в бою');
