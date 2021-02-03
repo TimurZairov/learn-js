3a41ed4a5ee9fc7afdaad4104e31fa38b864b582
Timur Zairov <zairovne@gmail.com>
2021-02-03 11:19:30 +0500

    lesson3

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-let money = 600;
-console.log(money);
+'use strict';
+let money = +prompt('Ваш месячный доход');
 let income = 600;
-console.log(income);
-let addExpenses = 'Обед, Метро, Интернет';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const deposit = confirm('Есть ли у вас депозит в банке?');
+const expenses1 = prompt('Введите обязательную статью расходов');
+const amount1 = +prompt('Во сколько это обойдется');
+const expenses2 = prompt('Введите обязательную статью расходов');
+const amount2 = +prompt('Во сколько это обойдется');
+let mission = 5000;
+
+
+const bugetMonth = (money - ((amount1)+(amount2)));
+const period = Math.ceil((mission)/(bugetMonth));
+let budgetDay = Math.trunc((bugetMonth)/30);
+
+console.log(`Ваш месячный доход ${money}`);
+console.log (addExpenses);
+console.log (deposit);
+console.log (`${bugetMonth} Бюджет на месяц`);
+console.log (`Цель достигнута будет за ${period}`);
+console.log (`${budgetDay} Бюджет на день`);
+/*console.log(income);
 console.log(addExpenses.length);
 console.log(addExpenses.split(','));
-console.log(addExpenses.toLowerCase());
-const deposit = true;
-console.log(deposit);
-let mission = 5000;
+console.log(addExpenses.toLowerCase());*/
 console.log('Цуль заработать' + ' ' + (mission) + ' ' + 'долларов');
-const period = 5;
 console.log('Период равен' + ' ' + (period)+ ' ' + 'месяцев');
 
-let budgetDay = (income)/30;
-console.log(budgetDay);
+if (budgetDay>=1200){
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay>600 || budgetDay<1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay>0 || budgetDay<600) {
+    console.log('К сожалению у вас уровень дохода ниже среднего')
+} else {
+    console.log('Что то пошло не так');
+};
